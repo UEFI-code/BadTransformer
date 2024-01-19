0f249aa0f989f3cff0f774d488ec3f2419de84e7
UEFI-code <zhuoshizhang@hotmail.com>
2024-01-19 16:00:27 +0800

    Refactor BadTransformerLLM.py to include multiple encoding layers and Tensor shape checks

--- BadTransformerLLM.py
+++ BadTransformerLLM.py
@@ -1,38 +1,45 @@
 import torch
 import torch.nn as nn
 class myBadTransfomerBlock(nn.Module):
-    def __init__(self, dim=4096, activation=nn.ReLU(), debug=False):
+    def __init__(self, dim=4096, deepth = 2, activation=nn.ReLU(), debug=False):
         super().__init__()
-        self.li1 = nn.Linear(dim, dim, bias=False)
-        self.li2 = nn.Linear(dim, dim, bias=False)
-        self.li3 = nn.Linear(dim, dim, bias=False)
-        self.li4 = nn.Linear(dim, dim, bias=False)
-        self.activation = activation
+        self.encodingGroupA = nn.Sequential()
+        self.encodingGroupB = nn.Sequential()
+        self.encodingGroupC = nn.Sequential()
+        self.decodingGroup = nn.Sequential()
+        for _ in range(deepth):
+            self.encodingGroupA.append(nn.Linear(dim, dim, bias=False))
+            self.encodingGroupA.append(activation)
+            self.encodingGroupB.append(nn.Linear(dim, dim, bias=False))
+            self.encodingGroupB.append(activation)
+            self.encodingGroupC.append(nn.Linear(dim, dim, bias=False))
+            self.encodingGroupC.append(activation)
         self.debug = debug
+        self.dim = dim
 
     def forward(self, x):
-        xA, xB, xC = self.li1(x), self.li2(x), self.li3(x)
-        xA, xB, xC = self.activation(xA), self.activation(xB), self.activation(xC)
+        xA, xB, xC = self.encodingGroupA(x), self.encodingGroupB(x), self.encodingGroupC(x)
         xSqure = torch.matmul(xA.transpose(1, 2), xB) # Here is to semantic hybrid. Expected shape: (batch, dim, dim)
+        if xSqure.shape[1] != self.dim or xSqure.shape[2] != self.dim:
+            raise ValueError(f'The shape of xSqure is not expected. Expected: (batch, {self.dim}, {self.dim}), Got: {xSqure.shape}. Maybe you should check the dimension of input.')
         if self.debug:
-            print(xSqure.shape)
+            print(f'Debug: xSqure shape: {xSqure.shape}')
         xO = torch.matmul(xC, xSqure) # Here is to re-mapping the meaning of each token embedding
-        return self.li4(xO)
+        return self.decodingGroup(xO)
 
 class myBadTransformerUnit(nn.Module):
-    def __init__(self, num_layers=2):
+    def __init__(self, dim = 4096, encodingDeepth = 2, num_layers=2, debug=False):
         super().__init__()
-        self.badtrans = []
+        self.badtrans = nn.Sequential()
         for _ in range(num_layers):
-            self.badtrans.append(myBadTransfomerBlock())
-        self.badtrans = nn.ModuleList(self.badtrans)
+            self.badtrans.append(myBadTransfomerBlock(dim=dim, deepth=encodingDeepth, debug=debug))
 
     def forward(self, x):
-        for badtrans in self.badtrans:
-            x = badtrans(x)
-        return x
+        return self.badtrans(x)
 
 if __name__ == "__main__":
     x = torch.randn(1, 16, 4096)
     badBlock = myBadTransfomerBlock(debug=True)
     print(badBlock(x))
+    badBlocks = myBadTransformerUnit(debug=True)
+    print(badBlocks(x))
