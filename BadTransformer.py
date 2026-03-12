b0ddd5b33ee365d8732a5a63c25333a95243b581
UEFI-code <zhuoshizhang@hotmail.com>
2026-03-12 22:23:16 +0900

    Fix Model

---
+++ BadTransformer.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import math
+
+class myBadTransfomerBlock(nn.Module):
+    def __init__(self, embdim=64):
+        super().__init__()
+        self.phase_A = nn.Linear(embdim, embdim, bias=True)
+        self.phase_B = nn.Linear(embdim, embdim, bias=True)
+        self.out_proj = nn.Linear(embdim, embdim, bias=True)
+        self.norm2 = nn.LayerNorm(embdim)
+
+    def forward(self, x):
+        A = self.phase_A(x)
+        B = self.phase_B(x)
+        attn = torch.matmul(A, B.transpose(1, 2))
+        attn = attn / math.sqrt(x.size(-1))
+        y = torch.matmul(attn, x)
+        y = self.out_proj(y)
+        y = self.norm2(y)
+        return y + x
+
+class myModel(nn.Module):
+    def __init__(self, max_seq_len = 128, embeddingDim = 64, num_layers=3):
+        super().__init__()
+        self.pre_embedding = nn.Embedding(257, embeddingDim)
+        self.positionEmbedding = nn.Parameter(torch.rand(1, max_seq_len, embeddingDim), requires_grad=True)
+        
+        self.badtrans = nn.Sequential()
+        for _ in range(num_layers):
+            self.badtrans.append(myBadTransfomerBlock(embdim=embeddingDim))
+        self.badtrans_deepth = num_layers
+        
+        self.windup = nn.Linear(embeddingDim, 256)
+
+    def forward(self, x):
+        x = self.pre_embedding(x) + self.positionEmbedding[:, :x.size(1)]
+        x = self.badtrans(x)
+        x = self.windup(x)
+        return x
+
+if __name__ == "__main__":
+    x = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+    badBlocks = myModel()
+    print(badBlocks(x))
